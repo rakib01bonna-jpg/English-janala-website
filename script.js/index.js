8037b2e337cb3a30dd1f75d9d670ce97ec8b55d5
Rakibul <ProgrammingHero1>
2026-03-09 23:36:38 +0600

    html push

--- script.js/index.js
+++ script.js/index.js
@@ -1,35 +1,30 @@
 const loadLessons = () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then((json) => console.log(json));
+    fetch("https://openapi.programming-hero.com/api/level/5") //promise of respnsive
+    .then(res => res.json()) // prmise of json date
+    .then((json) => displayLessons.log(json));
 };
-
 
 const displayLessons = (Lessons) => {
-    // console.log(Lessons);
-
-
     //1. get the conteiner & empty
-    const LevelConteiner = document.getElementById("level-conteiner");
-    LevelConteiner.innerHTML = "";
-    
-    //2. get into every lessson
-    for(let lesson of lesson) {
-      //3. crate Elment
-         const btnDiv = document.createElement("div");
-         btnDiv.document.createElement("div");
-         btnDiv.innerHTML = `
+         const LevelConteiner = document.getElementById("level-conteiner");
+         LevelConteiner.innerHTML = "";
+    //.2. get info every lessons
+    for (let lesson of Lessons) {
+        //3. create Element
+        console.log(lesson);
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `
+         <button onclick ="loadLEvelWord&({lesson.Level_no})" class="btn btn-outline btn-primey"> 
+            <i class="fa-solid fa-circle-question"></i>
+            lesson-${lesson.Level_no}</button>
 
-        <button ">
-             <i class="fa-solid fa-arrow-right-from-bracket"></i>FAG</=>
-
-            
-         `
-
-         // 4. append into container
-         LevelConteiner.append(btnDiv);
+        `
+          //4. append into conteiner
+       LevelConteiner.append(btnDiv);
     }
-    
-    
+   
 };
-loadLessons();
+
+ loadLessons();
+
+ console.log("all");
